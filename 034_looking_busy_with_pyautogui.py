185237bfd8455916113c2614e7204a31916864ff
victoradegoke54G4 <victoradegoke10@gmail.com>
2025-11-15 15:24:35 +0100

    Added project 34 with pyautogui

---
+++ 034_looking_busy_with_pyautogui.py
@@ -0,0 +1,19 @@
+import pyautogui, time, random
+
+
+
+def looking_busy(is_meeting = True):
+    """This function moves your mouse to avoid idleness"""
+    while is_meeting:
+
+        x = random.randint(-5,5)
+        y = random.randint(-5,5)
+        pyautogui.moveRel(x,y,2)
+        time.sleep(8)
+        
+        if x == y:
+            is_meeting = False
+
+    print('You are out of business.')
+if __name__ == '__main__':
+    looking_busy()
